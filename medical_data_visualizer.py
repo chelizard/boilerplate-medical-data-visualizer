7daf73acbf38941475d2d3b0454ab3b861d9910b
chelizard <chelseapmcmahon@gmail.com>
2025-03-21 14:38:52 -0700

    Updated boilerplate code with my solution for the medical data visualization with heatmap and catplot

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,58 +3,68 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
-# 2
-df['overweight'] = None
+BMI = df['weight'] / (df['height'] / 100)**2
+df['overweight'] = (BMI > 25).astype(int)
 
-# 3
+df[['cholesterol', 'gluc']] = (df[['cholesterol', 'gluc']] > 1).astype(int)
 
-
-# 4
-def draw_cat_plot():
-    # 5
-    df_cat = None
-
-
-    # 6
-    df_cat = None
+def draw_cat_plot(df):
+    # Melt DataFrame for categorical plot
+    df_cat = pd.melt(df, id_vars=["cardio"], 
+                      value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"])
     
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index()
+    df_cat.rename(columns={0: 'total'}, inplace=True)
+    
+    sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar')
+    plt.show()
 
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    # Get the figure for the output and store it in the fig variable.
+    fig = plt.figure(figsize=(10, 10))
+    # Save the figure and return it.
     fig.savefig('catplot.png')
     return fig
 
+def draw_heat_map(df):
+    # Clean the data
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) & 
+        (df['height'] >= df['height'].quantile(0.025)) & 
+        (df['height'] <= df['height'].quantile(0.975)) & 
+        (df['weight'] >= df['weight'].quantile(0.025)) & 
+        (df['weight'] <= df['weight'].quantile(0.975))]
+    
+    # Calculate the correlation matrix
+    corr = df_heat.corr()
+    
+    # Generate a mask for the upper triangle
+    mask = np.triu(corr)
+    
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(12, 12))
+    
+    # Plot the correlation matrix using sns.heatmap.
+    sns.heatmap(corr, mask=mask, center=0, vmin=-0.15, vmax=0.30, square=True, linewidths=.5, 
+                annot=True, fmt='.1f',
+                annot_kws={'size': 8},
+                cbar_kws={'shrink': 0.25,
+                          'ticks': [-0.08, 0.00, 0.08, 0.16, 0.24]},
+                          ax=ax)
+    plt.tight_layout()
+    plt.show()
 
-# 10
-def draw_heat_map():
-    # 11
-    df_heat = None
-
-    # 12
-    corr = None
-
-    # 13
-    mask = None
-
-
-
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
+    # Save the figure and return it.
     fig.savefig('heatmap.png')
     return fig
+    
+
+
+    
+# draw_cat_plot(df)
+# print(df['overweight'].value_counts())
+draw_heat_map(df)
+
+
+
